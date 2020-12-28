901b47d0a8dd3699affa7e01fd04136c0b99cd9d
artiume <siderite@gmail.com>
2020-12-28 10:09:59 -0500

    Update UserDataManager.cs

--- Emby.Server.Implementations/Library/UserDataManager.cs
+++ Emby.Server.Implementations/Library/UserDataManager.cs
@@ -219,7 +219,7 @@ namespace Emby.Server.Implementations.Library
             var hasRuntime = runtimeTicks > 0;
 
             // If a position has been reported, and if we know the duration
-            if (positionTicks > 0 && hasRuntime && !(item is book))
+            if (positionTicks > 0 && hasRuntime && !(item is Book))
             {
                 var pctIn = decimal.Divide(positionTicks, runtimeTicks) * 100;
 
