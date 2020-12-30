56d6f9bd2ee4efcc333d8ba79d22faec9bd0f489
artiume <siderite@gmail.com>
2020-12-30 05:46:24 -0500

    Update ServerConfiguration.cs

--- MediaBrowser.Model/Configuration/ServerConfiguration.cs
+++ MediaBrowser.Model/Configuration/ServerConfiguration.cs
@@ -301,13 +301,13 @@ namespace MediaBrowser.Model.Configuration
         /// Gets or sets the minimum minutes of a book that must be played in order for playstate to be updated.
         /// </summary>
         /// <value>The min resume in minutes.</value>
-        public int MinAudiobookResume { get; set; } = 10;
+        public int MinAudiobookResume { get; set; } = 5;
 
         /// <summary>
         /// Gets or sets the remaining minutes of a book that can be played while still saving playstate. If this percentage is crossed playstate will be reset to the beginning and the item will be marked watched.
         /// </summary>
         /// <value>The remaining time in minutes.</value>
-        public int MaxAudiobookResume { get; set; } = 10;
+        public int MaxAudiobookResume { get; set; } = 5;
 
         /// <summary>
         /// Gets or sets the minimum duration that an item must have in order to be eligible for playstate updates..
